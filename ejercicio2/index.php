4d3cb46f5f87e3225c164dfb8e5f1cdfed4b480c
Jefferson1LL <jefferson.rllumiquinga93@gmail.com>
2019-04-23 13:45:51 -0500

    ejercicio2

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -1,7 +1,39 @@
+ejercicio2
+<?php
+function frase(){
+    $texto = 'Ambientes No Propietarios Ejercicio 2 ';
+    for($i = 0 ; $i <= 2 ; $i++){
+        echo "$texto";
+        echo "<br>";
+    };
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+	    <meta charset="utf-8" />
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <title>Ejercicio 2</title>
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+	    <script src="main.js"></script>
+	</head>
+<body>
+ 
+
+<?php  
+echo "<div align=center>";
+echo frase();
+echo "</div><br>";
+?>
+
+</body>
+</html>
 <?php
 /**
  * Problema propuesto:
- *
- * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
+ * Implementar una función que muestre un título centrado en * pantalla, y la llamaremos posteriormente dos veces.
 */
 ?>
