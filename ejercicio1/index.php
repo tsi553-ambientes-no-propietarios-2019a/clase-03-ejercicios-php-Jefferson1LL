ad5811288da977c35bdc9eee3e139bb6ef9b6025
Jefferson1LL <jefferson.rllumiquinga93@gmail.com>
2019-04-23 13:42:45 -0500

    ejercicio1

--- ejercicio1/index.php
+++ ejercicio1/index.php
@@ -1,4 +1,33 @@
 <?php
+	
+	echo "/* ejemplo for */";
+	echo "<br>";
+	$n= 2;
+	for ($i=1; $i<11 ; $i++) {
+		
+		echo "$n X $i =" .$n * $i;
+		echo '<br>';
+		
+	}
+	echo "<br>";
+	echo "/* ejemplo while */";
+	echo "<br>";
+	$i = 1;
+	while ($i <= 10) {
+	    echo "$n X $i =" .$n * $i;
+		echo '<br>';
+		$i++; 
+	}
+	echo "<br>";
+	
+	echo"/* ejemplo do/while */";
+	echo '<br>';
+	$i = 1;
+	do {
+		echo "$n X $i =" .$n * $i;
+		echo '<br>';
+		$i++;
+	} while ( $i <= 10);
 /**
  * Problema propuesto.
  * Mostrar la tabla de multiplicar del 2. Emplear el for, luego el while y por último el do/while.
